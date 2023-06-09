b419da9dd6d9a5bab10735fa5aeadd380582fb74
Lucas Vinícius Vieira da Silva <lucvini56@gmail.com>
2023-06-09 13:15:30 -0300

    feat: Atualizações de estilo e funcionalidade do slider
    Agora os botões do slide têm suas cores ativadas de acordo com o número do slide. Além disso, a lógica para a mudança de cores nas setas do slider também foi definida.

--- js/index.js
+++ js/index.js
@@ -1,15 +1,52 @@
+// Declaração das variáveis
 let slider = document.querySelector('.slider')
 let setaEsquerda = document.querySelector('.seta-esquerda i')
-let setaDireita= document.querySelector('.seta-direita i')
+let setaDireita = document.querySelector('.seta-direita i')
+const botoes = document.querySelectorAll('.botao-slide');
 
 let moveSlider = 0
 
+// Cores padrão dos elementos do slider
+setaEsquerda.style.color = '#ccc'
+botoes[0].style.backgroundColor = '#fff'
+
+// Muda a cor dos botões do slider
+function ativaBotao(valor) {
+    for(let i = 0; i < botoes.length; i++) {
+        if(i == (Math.abs(valor)/100)) {
+            botoes[i].style.backgroundColor = '#fff';
+        } else {
+            botoes[i].style.backgroundColor = '#fff0';
+        }
+    }
+}
+
+// Funções para mudar os slides
 setaEsquerda.addEventListener('click', ()=> {
-    moveSlider += 100;
-    slider.style.marginLeft = moveSlider + 'vw'
+    if(moveSlider < 0) {
+        moveSlider += 100;
+        slider.style.marginLeft = moveSlider + 'vw'
+        setaDireita.style.color = '#000'
+    }
+
+    if(moveSlider == 0) {
+        setaEsquerda.style.color = '#ccc'
+    }
+
+    ativaBotao(moveSlider)
 })
 
 setaDireita.addEventListener('click', ()=> {
-    moveSlider -= 100;
-    slider.style.marginLeft = moveSlider + 'vw'
+    if(moveSlider > -200) {
+        moveSlider -= 100;
+        slider.style.marginLeft = moveSlider + 'vw'
+        setaEsquerda.style.color = '#000'
+    } 
+
+    if(moveSlider == -200) {
+        setaDireita.style.color = '#ccc'
+    }
+
+    ativaBotao(moveSlider)
 })
+
