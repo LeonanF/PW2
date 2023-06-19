099881dc633ee39b276c1cf8164fdbba7f1e7a2d
Leonan <91919730+LeonanF@users.noreply.github.com>
2023-06-19 15:29:27 -0300

    Update reset.js

--- js/reset.js
+++ js/reset.js
@@ -1,45 +1,11 @@
-// Seleciona os elementos do DOM
-const menuButton = document.querySelector('.menu');
-const nav = document.querySelector('nav');
-const closeMenuButton = document.querySelector('.fechar-menu');
-const pageContent = document.querySelector('body');
+const menuBtn = document.querySelector('.menu')
+const menuClose = document.querySelector('.fechar-menu')
+const nav = document.querySelector('nav')
 
-// Função para abrir o menu
-const openMenu = () => {
-  nav.style.display = 'flex';
-};
+menuBtn.addEventListener('click',()=>{
+  nav.style.display = 'flex'
+})
 
-// Função para fechar o menu
-const closeMenu = () => {
-  nav.style.display = 'none';
-  menuButton.style.display = 'flex';
-};
-
-// Função para fechar o menu quando um link é clicado
-const closeMenuOnLinkClick = () => {
-  closeMenu();
-};
-
-// Adiciona os event listeners aos elementos
-menuButton.addEventListener('click', openMenu);
-closeMenuButton.addEventListener('click', closeMenu);
-
-const menuLinks = document.querySelectorAll('nav ul li a');
-menuLinks.forEach(link => {
-  link.addEventListener('click', closeMenuOnLinkClick);
-});
-
-// Verifica o tamanho da tela ao carregar e redimensionar
-const checkScreenSize = () => {
-  if (window.innerWidth <= 650) {
-    closeMenu();
-    menuButton.style.display = 'flex';
-  } else {
-    nav.style.display = 'flex'
-    menuButton.style.display = 'none';
-    document.documentElement.style.overflow = 'auto';
-  }
-};
-
-window.addEventListener('load', checkScreenSize);
-window.addEventListener('resize', checkScreenSize);
+menuClose.addEventListener('click', ()=>{
+  nav.style.display = 'none'
+})
